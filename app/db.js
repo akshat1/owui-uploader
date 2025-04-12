0c347faa4b06862460771416d6922b4e203a93cb
Akshat Sharma <1529776+akshat1@users.noreply.github.com>
2025-04-11 23:43:10 -0700

    Updated the db schema.

--- app/db.js
+++ app/db.js
@@ -2,10 +2,10 @@
  * This module provides functions for our main logic to record and retrieve file metadata from a SQLite database.
  * The database is stored in the user's home directory as `.owui-sync.db`.
  * We need to store the following metadata for each file:
+ * - filePath (part of PRIMARY KEY)
+ * - knowledgeId (part of PRIMARY KEY)
  * - fileId
  * - lastModified
- * - path
- * - knowledgeId
  */
 
 const sqlite3 = require("sqlite3");
@@ -20,23 +20,34 @@ const initializeDB = async () => {
   db = new sqlite3.Database(path.join(process.env.HOME, ".owui-sync.db"));
   db.exec(` 
     CREATE TABLE IF NOT EXISTS files (
-      fileId TEXT PRIMARY KEY,
+      filePath TEXT,
+      knowledgeId TEXT,
+      fileId TEXT,
       lastModified TEXT,
-      path TEXT,
-      knowledgeId TEXT
+      PRIMARY KEY (filePath, knowledgeId)
     )
   `);
 };
 
-const recordFile = async (fileId, lastModified, path, knowledgeId) => {
+/**
+ * Make a record of a file uploaded to Open WebUI. Next time we sync, we'll
+ * check if the file has been modified. If so, we'll use the fileId to update
+ * the file.
+ * 
+ * @param {Object} args
+ * @param {string} args.fileId
+ * @param {string} args.lastModified
+ * @param {string} args.filePath
+ * @param {string} args.knowledgeId
+ */
+const recordFile = async ({ fileId, lastModified, filePath, knowledgeId }) => {
   await initializeDB();
-  console.log(">>>", db.exec._foo, db.exec);
-  db.exec("INSERT INTO files (fileId, lastModified, path, knowledgeId) VALUES (?, ?, ?, ?);", [fileId, lastModified, path, knowledgeId]);
+  db.exec("INSERT OR REPLACE INTO files (filePath, knowledgeId, fileId, lastModified) VALUES (?, ?, ?, ?);", [filePath, knowledgeId, fileId, lastModified]);
 };
 
-const getFile = async (fileId) => {
+const getFile = async (filePath, knowledgeId) => {
   await initializeDB();
-  const result = await db.get("SELECT * FROM files WHERE fileId = ?;", [fileId]);
+  const result = await db.get("SELECT * FROM files WHERE filePath = ? AND knowledgeId = ?;", [filePath, knowledgeId]);
   return result;
 };
 
@@ -46,9 +57,9 @@ const getFiles = async (knowledgeId) => {
   return result;
 };
 
-const deleteFile = async (fileId) => {
+const deleteFile = async (filePath, knowledgeId) => {
   await initializeDB();
-  db.exec("DELETE FROM files WHERE fileId = ?;", [fileId]);
+  db.exec("DELETE FROM files WHERE filePath = ? AND knowledgeId = ?;", [filePath, knowledgeId]);
 };
 
 module.exports = {
